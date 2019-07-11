c7d6ae90a73a5e881c47797c7652f6b7a18b4bae
ramson <ramson@10.1.10.209>
2019-07-10 22:27:16 -0400

    adding html header and footer common,setting up a login and signup pages and mapping to the home controller

--- E-banking/src/main/java/com/eBanking/controller/HomeController.java
+++ E-banking/src/main/java/com/eBanking/controller/HomeController.java
@@ -1,7 +1,16 @@
 package com.eBanking.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.eBanking.entity.User;
 
 @Controller
 public class HomeController {
@@ -14,5 +23,38 @@ public class HomeController {
 	@RequestMapping("/index")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/signup")
+	public String signup(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		
+		return "signup";
+	}
+	
+	@PostMapping("/signup")
+	public void signupPost(@ModelAttribute("user") User user, Model model) {
+		
+//		if(userService.checkUserExists(user.getUsername(),user.getEmail())) {
+//			
+//			if(userService.checkEmailExists(user.getEmail())) {
+//				model.addAttribute("emailExists", true);
+//			}
+//			
+//			if(userService.checkUsernameExists(user.getUsername())) {
+//				model.addAttribute("usernameExists", true);
+//			}
+//			
+//			return "signup";
+//		}else {
+//			Set<UserRole> userRoles = new HashSet<>();
+//			userRoles.add(new UserRole(user,roleDao.findByname("USER")))model;
+//			userService.createUser(user,userRoles);
+//			
+//			return "redirect:/";
+//		}
+		
+		
 	}
 }
